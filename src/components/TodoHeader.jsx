56998c68274bee897d3496f517b0e3cbac7f5ef7
MSKose <saitmustafa1998@gmail.com>
2022-06-29 17:11:23 +0200

    first commit

---
+++ src/components/TodoHeader.jsx
@@ -0,0 +1,35 @@
+// import React from 'react'
+
+// const TodoHeader = () => {
+//   return (
+//     <a
+//         href="/#"
+//         className="list-group-item list-group-item-secondary d-flex flex-column align-items-center"
+//         aria-current="true"
+//       >
+//       <div className="d-flex w-100 justify-content-between"></div>
+//       <h1 className="m-3 text-center text-uppercase">My to-do list</h1>
+//       <div className="input-group mb-3 w-75">
+//       <button
+//         className="btn btn-outline-primary hover-shadow"
+//         type="submit"
+//         id="button-addon1"
+//       >
+//         Add Item
+//       </button>
+//       <input
+//         type="text"
+//         className="form-control"
+//         id="textAdder"
+//         aria-label="Example text with button addon"
+//         aria-describedby="button-addon1"
+//         placeholder="Please type a to-do"
+//         value={todo}
+//         onChange={(e) => setTodo(e.target.value)}
+//       />
+//       </div>
+//       </a>
+//   )
+// }
+
+// export default TodoHeader;
---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,25 @@
+// import React from 'react'
+
+// const TodoItems = (todo) => {
+//   return (
+//     <div>
+//         <a href="/#" className="list-group-item list-group-item-action d-flex m-auto">
+//                 <input
+//                   className="form-check-input mt-auto mb-auto"
+//                   type="checkbox"
+//                   defaultValue=""
+//                   aria-label="Checkbox for following text input"
+//                 />
+//                 <p className="m-auto fs-5 text-capitalize overflow-auto" id="textP">
+//                   {todo.text}
+//                 </p>
+//                 <button type="button" className="btn btn-danger" id="removeBtn">
+//                   Remove
+//                 </button>
+//             </a>
+//             <div>wekjfbkwej</div>
+//     </div>
+//   )
+// }
+
+// // export default TodoItems
